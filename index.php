b0c3e8b3e07c9cd2aef29c6509f95ee012a4a4fd
Kristijan19 <kristijan.konda16@gmail.com>
2025-10-29 19:59:32 +0100

    Index view

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
 
-$business = [
-        'name' => 'Leracasts',
-        'cost' => 15,
-        'categories' => ["Testing", "PHP", "JavaScript"]
- ];
-
-function register($user) {
-    // Create user record in the db.
-    // Log them in.
-    // Send a welcome email.
-    // Redirect to their new dashboard
-
-}
-
 require "index.view.php";
