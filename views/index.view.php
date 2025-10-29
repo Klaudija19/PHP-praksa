b2761c4ef15ab074d87693b61fd71886f5e6eff7
Kristijan19 <kristijan.konda16@gmail.com>
2025-10-29 23:29:42 +0100

    New folders and files

---
+++ views/index.view.php
@@ -0,0 +1,33 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
+
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <p>Hello. Welcome to the home page. </p>
+        </div>
+    </main>
+
+<?php require('partials/footer.php') ?>
+
+
+<!-- JavaScript за dropdown -->
+<script>
+    const userButton = document.getElementById('user-menu-button');
+    const dropdown = document.getElementById('dropdown-menu');
+
+    userButton.addEventListener('click', (e) => {
+        e.stopPropagation();
+        dropdown.classList.toggle('hidden');
+    });
+
+    // затворање при клик надвор
+    document.addEventListener('click', (e) => {
+        if (!dropdown.contains(e.target) && !userButton.contains(e.target)) {
+            dropdown.classList.add('hidden');
+        }
+    });
+</script>
+
+
