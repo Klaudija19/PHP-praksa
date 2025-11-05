a1336c33a1c7ef5d06fc62ff08ed53e80b5c4a25
Kristijan19 <kristijan.konda16@gmail.com>
2025-11-05 11:13:25 +0100

    SQL injection vulnerabilities

--- Database.php
+++ Database.php
@@ -5,17 +5,23 @@ class Database
 
     public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = 'mysql:' . http_build_query($config, '', ';');
+        // Создавање на DSN за MySQL
+        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
 
-        $this->connection = new PDO($dsn, $username, $password), [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        // Креирање на PDO конекција со опции
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
+        $this->connection = new PDO($dsn, $username, $password, $options);
     }
 
     public function query($query)
     {
         $statement = $this->connection->prepare($query);
         $statement->execute();
-
+        return $statement;
     }
 }
+
--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
-
-require 'functions.php';
 require 'Database.php';
 
-$config = require('config.php');
+$config = [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'myapp',
+    'charset' => 'utf8mb4',
+    'user' => 'root',
+    'pass' => ''
+];
 
-$db = new Database($config ['database']);
+$db = new Database($config);
 
-// Пребарувај ја табелата posts
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
+// Еден пост со id = 1
+$id = 1;
+$post = $db->query("SELECT * FROM posts WHERE id = $id")->fetch();
 
-// Прикажи ја содржината на табелата
-echo '<pre>';
-print_r($posts);
-echo '</pre>';
+// Сите users од app_users
+$users = $db->query("SELECT * FROM app_users")->fetchAll();
+
+// Прикажи резултати
+echo "<h2>Post with ID = 1</h2>";
+echo "<pre>";
+print_r($post);
+echo "</pre>";
+
+echo "<h2>All Users</h2>";
+echo "<pre>";
+print_r($users);
+echo "</pre>";
 
 
