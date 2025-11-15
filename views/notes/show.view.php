11ba6880f020f8248e9c298645bd304053954da0
Kristijan19 <kristijan.konda16@gmail.com>
2025-11-15 15:53:12 +0100

    Handle multiple request methods

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -8,6 +8,12 @@ require basePath('views/partials/banner.php');
 <main>
     <h1><?= htmlspecialchars($note['body'] ?? 'Note not found') ?></h1>
 
+    <!-- Delete form -->
+    <form method="POST">
+        <input type="hidden" name="id" value="<?= $note['id'] ?>">
+        <button type="submit" class="text-red-500 text-sm mt-2">Delete</button>
+    </form>
+
     <!-- Back копче -->
     <p>
         <a href="/notes" style="color: blue; text-decoration: underline;">&larr; Go Back</a>
